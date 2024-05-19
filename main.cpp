ab2792cd64d10c1ce636861cb4f7f8b732e72bce
George Krovsh <gosha.krovsh@gmail.com>
2024-05-19 17:16:47 +0300

    Fix example

--- main.cpp
+++ main.cpp
@@ -8,23 +8,23 @@ int main() {
         }, settings
     );
 
-    auto car = db->Find("CAR", "BMW");
+    auto car = db->Find("CAR", "BMW X6");
     if (!car.has_value()) {
-        db->Put("CAR", "BMW", "X6");
-        db->Put("PRICE", "BMW", "100");
+        db->Put("CAR", "BMW X6", "Black");
+        db->Put("PRICE", "BMW X6", "100");
     }
 
     auto read_tx = db->newReadTx({ "CAR" });
-    auto bmw_result = read_tx->Find("CAR", "BMW");
-    auto mercedes_result = read_tx->Find("CAR", "Mercedes");
+    auto bmw_result = read_tx->Find("CAR", "BMW X7");
+    auto mercedes_result = read_tx->Find("CAR", "Mercedes GL");
     // ...
     read_tx->commit();
 
     auto tx = db->newWriteTx({ "CAR", "PRICE" });
     try {
-        tx->Remove("CAR", "BMW");
-        tx->Put("CAR", "Mercedes", "CLS");
-        tx->Put("PRICE", "Mercedes", "200");
+        tx->Remove("CAR", "BMW X6");
+        tx->Put("CAR", "Mercedes CLS", "White");
+        tx->Put("PRICE", "Mercedes CLS", "200");
         // ...
         throw std::runtime_error("Some runtime error");
     } catch (...) {
